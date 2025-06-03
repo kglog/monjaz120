acb74cdefb4b06f937d123cb2df3cfad6bc35edb
Kahilan <dfgnhfgdrg@gmail.com>
2025-06-03 07:51:56 +0300

    feat: add firebaseConfig file

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -1,14 +1,13 @@
-// src/lib/firebaseConfig.ts
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID'
+  apiKey: "xxx",
+  authDomain: "xxx",
+  projectId: "xxx",
+  storageBucket: "xxx",
+  messagingSenderId: "xxx",
+  appId: "xxx"
 };
 
 const app = initializeApp(firebaseConfig);
