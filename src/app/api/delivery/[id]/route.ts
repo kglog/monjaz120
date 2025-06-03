41c2289c78bb20585dedcd0168466f10772cb876
Kahilan <dfgnhfgdrg@gmail.com>
2025-06-03 06:59:49 +0300

    fix: add delivery/[id]/route.ts correctly

---
+++ src/app/api/delivery/[id]/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get('id');
+
+  return NextResponse.json({ id });
+}
