c8e4dd945fb91ca1bbbaa9a9e5e54a24a78fca79
kglog <dfgnhfgdrg@gmail.com>
2025-10-04 06:30:09 +0300

    إضافة صفحة بيانات أساسية للتحقق من الهوية.

---
+++ src/app/account/verify/basic-info/page.tsx
@@ -0,0 +1,13 @@
+// محتوى صفحة البيانات الأساسية للتحقق من الهوية
+import React from 'react';
+
+const BasicInfoPage = () => {
+    return (
+        <div>
+            <h1>البيانات الأساسية للتحقق من الهوية</h1>
+            {/* إضافة مكونات أخرى هنا */}
+        </div>
+    );
+};
+
+export default BasicInfoPage;
